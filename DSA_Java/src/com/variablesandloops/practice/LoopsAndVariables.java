0e15c0c9a0471b812568b2d9228822e142c09aab
manimala-nayak <manimala.nayak05@gmail.com>
2025-02-12 10:33:51 +0530

    Variable Program

---
+++ DSA_Java/src/com/variablesandloops/practice/LoopsAndVariables.java
@@ -0,0 +1,12 @@
+package com.variablesandloops.practice;
+
+public class LoopsAndVariables {
+
+	public static void main(String[] args) {
+		int a = 10;
+		float b = 1098.988f;
+		//The sum is casted to float type since float bit value is higher.
+		System.out.println("The sum is : "+ a+b);
+	}
+
+}
