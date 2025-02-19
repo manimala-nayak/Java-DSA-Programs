be9d2a50b2da92b6b89721ec291e0d22df77172f
manimala-nayak <manimala.nayak05@gmail.com>
2025-02-19 20:57:33 +0530

    Array Swap

---
+++ DSA_Java/src/arrays/ArraySwapNumbers.java
@@ -0,0 +1,35 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArraySwapNumbers {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter first index: ");
+		int index1 = in.nextInt();
+		System.out.println("Enter second index: ");
+		int index2 = in.nextInt();
+		int[] arr = {1,8,4,10,56,23};
+		swap(index1,index2,arr);
+
+	}
+
+	  static void swap(int index1, int index2, int[] arr) {
+		int temp = 0;
+		System.out.println("The first array");
+		System.out.println(Arrays.toString(arr));
+		
+		temp = arr[index1];
+		arr[index1] = arr[index2];
+		arr[index2]=temp;
+		
+		System.out.println("The second array");
+		System.out.println(Arrays.toString(arr));
+		
+	}
+
+	
+}
