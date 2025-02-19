f29acd4c96eeeb26d048f88d0cd13adaa2059bf3
manimala-nayak <manimala.nayak05@gmail.com>
2025-02-19 21:06:06 +0530

    Array Swap after closing the in object

--- DSA_Java/src/arrays/ArraySwapNumbers.java
+++ DSA_Java/src/arrays/ArraySwapNumbers.java
@@ -6,7 +6,9 @@ import java.util.Scanner;
 public class ArraySwapNumbers {
 
 	public static void main(String[] args) {
-		
+		/*
+		 * ArraySwapNumbers ab = new ArraySwapNumbers(); System.out.println(ab);
+		 */
 		Scanner in = new Scanner(System.in);
 		System.out.println("Enter first index: ");
 		int index1 = in.nextInt();
@@ -14,7 +16,9 @@ public class ArraySwapNumbers {
 		int index2 = in.nextInt();
 		int[] arr = {1,8,4,10,56,23};
 		swap(index1,index2,arr);
-
+		System.out.println(in.toString());
+        in.close();
+        System.out.println(in.toString());
 	}
 
 	  static void swap(int index1, int index2, int[] arr) {
